3e3b5b82792adb0d80800c4585cec8f081dde729
MarioGotta <mario.lucio57@gmail.com>
2023-12-14 21:24:42 -0300

    .

--- Exercicios/Desafio21.py
+++ Exercicios/Desafio21.py
@@ -1,2 +1,9 @@
 # Faça um programa em python que abra e reproduza um áudio de um arquivo MP3.
+
 import pygame
+pygame.init()
+
+pygame.mixer.music.load('Desafio21.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
